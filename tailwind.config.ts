bf062f4d91eaf3c531a25eba1b65d56ee1337bc5
Emily Zhai <ezhai24@gmail.com>
2023-10-24 17:04:17 -0400

    configure theme in tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        md: "800px",
+      },
+      colors: {
+        blue: "#05B8B9",
+        pink: "#FC5CB7",
+        gray: "#F5F5F5",
+      },
+      fontFamily: {
+        trailers: ["TT Trailers, sans-serif"],
+        poller: ["Poller One, sans-serif"],
+        sans: ["Kumbh Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
