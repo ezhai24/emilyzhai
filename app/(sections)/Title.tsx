fec429444b310762e236a4e75b47c0134ba5cefc
Emily Zhai <ezhai24@gmail.com>
2023-10-26 13:48:46 -0400

    fit title screen + drips to screen height more responsively

--- app/(sections)/Title.tsx
+++ app/(sections)/Title.tsx
@@ -11,36 +11,38 @@ export const Title = () => {
   useMotionValueEvent(scrollYProgress, "change", (y) => setScrollY(y));
 
   return (
-    <section className="bg-blue relative z-10 px-20 pb-2 pt-24 text-center">
-      <h2 className="mb-4">EMILY ZHAI</h2>
-      <h1 className="mb-3">FULL-STACK SOFTWARE BUILT FOR HUMANS</h1>
-      <p className="mx-auto w-2/3">
-        My name is Emily. I build scalable, performant technology that solves
-        real problems for real people.
-      </p>
-      <motion.button initial="default" whileHover="hover" className="mt-10">
-        <div className="rounded-full border-2 px-4 py-1 font-bold">
-          here&apos;s proof
-        </div>
-        <motion.div
-          variants={{
-            hover: {
-              y: 4,
-              transition: {
-                repeat: Infinity,
-                repeatType: "mirror",
-                duration: 0.35,
+    <section className="bg-blue relative z-10 flex min-h-[90vh] items-center px-20 pt-24">
+      <div className="text-center">
+        <h2 className="mb-4">EMILY ZHAI</h2>
+        <h1 className="mb-3">FULL-STACK SOFTWARE BUILT FOR HUMANS</h1>
+        <p className="mx-auto w-2/3">
+          My name is Emily. I build scalable, performant technology that solves
+          real problems for real people.
+        </p>
+        <motion.button initial="default" whileHover="hover" className="mt-10">
+          <div className="rounded-full border-2 px-4 py-1 font-bold">
+            here&apos;s proof
+          </div>
+          <motion.div
+            variants={{
+              hover: {
+                y: 4,
+                transition: {
+                  repeat: Infinity,
+                  repeatType: "mirror",
+                  duration: 0.35,
+                },
               },
-            },
-          }}
-        >
-          <FaChevronDown className="mt-0.5 w-full text-center" />
-        </motion.div>
-      </motion.button>
+            }}
+          >
+            <FaChevronDown className="mt-0.5 w-full text-center" />
+          </motion.div>
+        </motion.button>
+      </div>
 
       <Drip
-        initial={{ scaleY: Math.min(0.5 + 2 * scrollY, 1.25) }}
-        animate={{ scaleY: Math.min(0.5 + 2 * scrollY, 1.25) }}
+        initial={{ scaleY: Math.min(0.5 + 2.5 * scrollY, 1.3) }}
+        animate={{ scaleY: Math.min(0.5 + 2.5 * scrollY, 1.3) }}
         fill="rgb(var(--color-blue))"
         className="pointer-events-none absolute -bottom-28 left-0 -z-10 h-auto w-screen md:-bottom-48"
       />
